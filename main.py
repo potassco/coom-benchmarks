dcdb4c4c5118964242e9d6cd58adf06d50302126
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2023-06-26 14:37:19 +0200

    support for non-numeric option values

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class ASPVisitor(ModelVisitor):
         super().__init__()
         self.parent_enum: Optional[ModelParser.EnumerationContext] = None
         # self.parent_behavior: Optional[ModelParser.BehaviorContext] = None
-        self.behavior_name: str = '":root"'
+        self.behavior_name: str = ':root'
         self.constraint_idx: int = 0
         # self.path_idx: int = 0
         self.row_idx: int = 0
@@ -21,16 +21,16 @@ class ASPVisitor(ModelVisitor):
         self.print_path: bool = True
 
     def visitProduct(self, ctx: ModelParser.ProductContext):
-        print("structure(\":root\").")
+        print('structure(":root").')
         super().visitProduct(ctx)
 
     def visitStructure(self, ctx: ModelParser.StructureContext):
-        print(f"structure({ctx.name().getText()}).")
+        print(f'structure("{ctx.name().getText()}").')
         super().visitStructure(ctx)
 
     def visitEnumeration(self, ctx: ModelParser.EnumerationContext):
         self.parent_enum = ctx
-        print(f"enumeration(\"{ctx.name().getText()}\").")
+        print(f'enumeration("{ctx.name().getText()}").')
         super().visitEnumeration(ctx)
         self.parent_enum = None
 
@@ -51,41 +51,41 @@ class ASPVisitor(ModelVisitor):
         #         0).condition_not(0).condition_compare().condition_part(
         #             0).getText())
         super().visitBehavior(ctx)
-        self.behavior_name = '":root"'
+        self.behavior_name = ':root'
 
     def visitFeature(self, ctx: ModelParser.FeatureContext):
         field: ModelParser.FieldContext = ctx.field()
         if field.number_def() is not None:
-            type_name = "num"
+            type_name = 'num'
         elif field.string_def() is not None:
-            type_name = "text"
+            type_name = 'text'
         else:
             # this seems hacky, I couldn't figure out how to do it better on the hurry
             try:
-                type_name = '"' + field.type_ref().name().getText() + '"'
+                type_name = field.type_ref().name().getText()
             except TypeError:
-                type_name = '":root"'
-        feature_name = '"' + field.fieldName.getText() + '"'
+                type_name = ':root'
+        feature_name = field.fieldName.getText()
         cardinality: ModelParser.CardinalityContext = ctx.cardinality()
         c_min = 1 if cardinality is None else cardinality.min
         c_max = 1 if cardinality is None else cardinality.max
-        print(f"feature({type_name}, {feature_name}, {c_min}, {c_max}).")
+        print(f'feature("{type_name}", "{feature_name}", {c_min}, {c_max}).')
 
     def visitAttribute(self, ctx: ModelParser.AttributeContext):
         if self.parent_enum is None:
             raise ValueError("illegal option")
-        parent_name = '"' + self.parent_enum.name().getText() + '"'
+        parent_name = self.parent_enum.name().getText()
         field: ModelParser.FieldContext = ctx.field()
-        field_name = '"' + field.fieldName.getText() + '"'
-        print(f'attribute({parent_name},{field_name}).')
+        field_name = field.fieldName.getText()
+        print(f'attribute("{parent_name}","{field_name}").')
         super().visitAttribute(ctx)
 
     def visitOption(self, ctx: ModelParser.OptionContext):
         if self.parent_enum is None:
             raise ValueError("illegal option")
-        parent_name = '"' + self.parent_enum.name().getText() + '"'
-        option_name = '"' + ctx.name().getText() + '"'
-        print(f"option({parent_name}, {option_name}).")
+        parent_name = self.parent_enum.name().getText()
+        option_name = ctx.name().getText()
+        print(f'option("{parent_name}", "{option_name}").')
 
         constant: ModelParser.ConstantContext = ctx.constant()
         if constant != []:
@@ -93,30 +93,31 @@ class ASPVisitor(ModelVisitor):
             )
             for a, c in zip(parent_attr, constant):
                 field: ModelParser.FieldContext = a.field()
-                attr_name = '"' + field.fieldName.getText() + '"'
-                option_value = c.floating().getText()
+                attr_name = field.fieldName.getText()
+                if c.floating() is not None:
+                    option_value = c.floating().getText()
+                elif c.name() is not None:
+                    option_value = f'"{c.name().getText()}"'
                 print(
-                    f'value({parent_name},{option_name},{attr_name},{option_value}).'
+                    f'value("{parent_name}","{option_name}","{attr_name}",{option_value}).'
                 )
 
     def visitConditioned(self, ctx: ModelParser.ConditionedContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        constraint_id = f'("{self.behavior_name}",{self.constraint_idx})'
         print(f'\nbehavior({constraint_id}).')
         super().visitConditioned(ctx)
         self.constraint_idx += 1
 
     def visitCombinations(self, ctx: ModelParser.CombinationsContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        constraint_id = f'("{self.behavior_name}",{self.constraint_idx})'
         for i, f in enumerate(ctx.formula()):
-            p = f'"{f.getText()}"'
-            print(f'combinations({constraint_id},{i},{p}).')
+            print(f'combinations({constraint_id},{i},"{f.getText()}").')
         super().visitCombinations(ctx)
         self.row_idx = 0
 
     def visitCombination_row(self, ctx: ModelParser.Combination_rowContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        constraint_id = f'("{self.behavior_name}",{self.constraint_idx})'
         type = ctx.rowType.text
-        print(ctx.getText())
         for col_idx, item in enumerate(ctx.combination_item()):
             values = item.getText()
             # Removing brackets around the values. Is this safe?
@@ -124,20 +125,21 @@ class ASPVisitor(ModelVisitor):
                 values = values[1:-1]
             for v in values.split(','):
                 print(
-                    f'{type}({constraint_id},({col_idx},{self.row_idx}),{v}).')
+                    f'{type}({constraint_id},({col_idx},{self.row_idx}),"{v}").'
+                )
         self.print_path = False
         super().visitCombination_row(ctx)
         self.print_path = True
         self.row_idx += 1
 
     def visitPrecondition(self, ctx: ModelParser.PreconditionContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        constraint_id = f'("{self.behavior_name}",{self.constraint_idx})'
         condition = f'"{ctx.condition().getText()}"'
         print(f'condition({constraint_id},{condition}).')
         super().visitPrecondition(ctx)
 
     def visitRequire(self, ctx: ModelParser.RequireContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        constraint_id = f'("{self.behavior_name}",{self.constraint_idx})'
         condition = f'"{ctx.condition().getText()}"'
         print(f'require({constraint_id},{condition}).')
         super().visitRequire(ctx)
@@ -145,7 +147,7 @@ class ASPVisitor(ModelVisitor):
 
     def visitCondition_compare(self,
                                ctx: ModelParser.Condition_compareContext):
-        constraint_id = f'({self.behavior_name},{self.constraint_idx})'
+        # constraint_id = f'("{self.behavior_name}"",{self.constraint_idx})'
         # condition = f'"{ctx.condition().getText()}"'
         # print(ctx.formula().getText())
         # for p in ctx.condition_part():
@@ -158,8 +160,7 @@ class ASPVisitor(ModelVisitor):
         if self.print_path:
             full_path = f'"{ctx.getText()}"'
             for i, p in enumerate(ctx.path_item()):
-                path = f'"{p.getText()}"'
-                print(f'path({full_path},{i},{path}).')
+                print(f'path({full_path},{i},"{p.getText()}").')
 
     # def visitFormula_atom(self, ctx: ModelParser.Formula_atomContext):
     #     print('\n')
@@ -187,7 +188,7 @@ class ASPVisitor(ModelVisitor):
     #     print(formula)
     #     for summand in ctx.formula_sub():
     #         print(summand.getText())
-    #         # print(f'add({formula},{self.behavior_name},{term1}).')
+    #         # print(f'add({formula},"{self.behavior_name}"",{term1}).')
     #     super().visitFormula_add(ctx)
 
     # def visitFormula_sub(self, ctx: ModelParser.Formula_subContext):
