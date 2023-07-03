f9beb17ae63cf654d6fc499cacd017d40e137715
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2023-07-03 16:27:05 +0200

    added more tests

--- tests/tests_parser.py
+++ tests/tests_parser.py
@@ -20,6 +20,37 @@ class TestMain(unittest.TestCase):
             'feature(":root","frame","Frame",1,1).'
         ])
 
+    def test_enumeration(self):
+        self.assertEqual(
+            parse_coom('enumeration Color {Red Green Yellow Blue}'), [
+                'enumeration("Color").', 'option("Color", "Red").',
+                'option("Color", "Green").', 'option("Color", "Yellow").',
+                'option("Color", "Blue").'
+            ])
+        self.assertEqual(
+            parse_coom('''\
+                enumeration Capacity {
+                        attribute num/l volume
+                        attribute num/gr weight
+                        B10  = ( 10,  100)
+                        B20  = ( 20,  250)
+                    }'''), [
+                'enumeration("Capacity").', 'attribute("Capacity","volume").',
+                'attribute("Capacity","weight").',
+                'option("Capacity", "B10").',
+                'value("Capacity","B10","volume",10).',
+                'value("Capacity","B10","weight",100).',
+                'option("Capacity", "B20").',
+                'value("Capacity","B20","volume",20).',
+                'value("Capacity","B20","weight",250).'
+            ])
+
+    def test_behavior(self):
+        self.assertEqual(parse_coom('behavior{require a = b}'), ['behavior((":root",0))'])
+
+    def test_multiplicity(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
