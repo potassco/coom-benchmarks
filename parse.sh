e63007de18327e2a6330d4fcbe3817c23ff12fec
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2024-01-19 17:54:26 -0300

    added parse script. further organized coom model

---
+++ parse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+parse_file=parser/parse.py
+asp_dir=generated
+
+instance_name=$(basename $1)
+outfile=$asp_dir/${instance_name/.coom/.lp}
+
+if [ ! -d $asp_dir ]
+then
+    mkdir $asp_dir
+fi
+
+if [ -e $1 ]
+then
+    python $parse_file $1 > $outfile
+fi
--- tests/__init__.py
+++ tests/__init__.py
@@ -5,10 +5,10 @@ from io import StringIO
 from unittest.mock import patch
 
 from antlr4 import InputStream, CommonTokenStream
-from model.ModelLexer import ModelLexer
-from model.ModelParser import ModelParser
+from parser.model.ModelLexer import ModelLexer
+from parser.model.ModelParser import ModelParser
 
-from parse import ASPVisitor
+from parser.parse import ASPVisitor
 
 
 def parse_coom(coom_input):
