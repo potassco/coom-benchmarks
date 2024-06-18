a57b60788bc9cd6346c564ebac4b8c436179e42c
Nicolas Rühling <82144991+nrueh@users.noreply.github.com>
2024-06-18 12:00:01 -0300

    modified travel bike fleet instances

--- benchmarks/domain/travel-bike-fleet/create_instances.sh
+++ benchmarks/domain/travel-bike-fleet/create_instances.sh
@@ -7,8 +7,12 @@ NAME=travelbike
 rm -rf $OUTDIR
 mkdir $OUTDIR
 
-for numbikes in $(seq 1 5)
-do
-    OUTFILE=$OUTDIR/${NAME}${numbikes}.coom
-    sed -e "s/NUMBIKES/${numbikes}/g" $TEMPLATE > $OUTFILE
+MAX_PER_BIKE=500
+
+for numbikes in $(seq 1 5); do
+    for maxprice in $(seq 200 100 500); do
+        MAXPRICE_TOTAL=$(($maxprice*numbikes))
+        OUTFILE=$OUTDIR/${NAME}${numbikes}_${MAXPRICE_TOTAL}.coom
+        sed -e "s/NUMBIKES/${numbikes}/g" -e "s/MAXPRICE/${MAXPRICE_TOTAL}/g" $TEMPLATE > $OUTFILE
+    done
 done
