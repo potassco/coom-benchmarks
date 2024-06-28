66c645e14d70c76c99f30f979d4d2faa78dcef7c
Nicolas Rühling <nruehling@uni-potsdam.de>
2024-06-28 02:15:13 +0200

    added new ods

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -5,22 +5,20 @@ source scripts/vars.env
 echo "Entering benchmark-tool directory"
 cd $BT_DIR
 
-# echo "Cleaning up old results"
-# rm -rf $RESULTS_DIR
-# mkdir $RESULTS_DIR
+echo "Cleaning up old results"
+rm -rf $RESULTS_DIR
+mkdir $RESULTS_DIR
 
 echo "Evaluating results"
 for rs in $RUNSCRIPT_PATH; do
-    eval=${RESULTS_DIR}/$(echo $rs | cut -d'-' -f 4)
+    eval=$(echo $rs | cut -d'-' -f 4)
     name=${eval%.*}
-    echo "Calculating ${eval}"
-    ./beval $RUNSCRIPT_PATH > ${eval} 2> ${name}.error
-done
 
-# ODS_FILE=${RESULTS_DIR}/${EVAL_NAME}.ods
-echo "Computing .ods files"
-for eval in $(ls $RESULTS_DIR); do
-    if [[ "${eval}" == *.xml ]]; then
-        name=${eval%.*}
-        ./bconv -m time,models,timeout,error,memout ${eval} > ${name}.ods 2> ${name}.error
-    fi
+    echo "Calculating ${name}"
+    eval_file=${RESULTS_DIR}/${eval}
+    ./beval $rs > ${eval_file} 2> ${eval_file}.error
+
+    echo "Computing .ods file"
+    ods_file=${RESULTS_DIR}/${name}.ods
+    ./bconv -m time,models,timeout,error,memout ${eval_file} > ${ods_file} 2> ${ods_file}.error
+done
