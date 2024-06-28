e28f2c4c3877d9d5af18e7b394d2e14c5b40635d
Nicolas Rühling <82144991+nrueh@users.noreply.github.com>
2024-06-28 11:43:47 -0300

    updated core instances

--- benchmarks/domain/random-core/create_instances.sh
+++ benchmarks/domain/random-core/create_instances.sh
@@ -5,8 +5,8 @@ NAME=randomcore
 
 rm -rf $OUTDIR
 
-for numfeat in 50 100; do
-    for numopt in 50 100 150; do
+for numfeat in 50 150 250; do
+    for numopt in 50 150 250; do
         for consize in 2 3 4; do
             python generate.py -f $numfeat -o $numopt -c $consize --name $NAME --out $OUTDIR
         done
