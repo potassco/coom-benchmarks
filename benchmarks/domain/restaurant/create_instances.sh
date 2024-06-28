9992368939641a8cc4e357be9beda46d8c7634e6
Nicolas Rühling <82144991+nrueh@users.noreply.github.com>
2024-06-28 11:17:45 -0300

    updated restaurant instances

--- benchmarks/domain/restaurant/create_instances.sh
+++ benchmarks/domain/restaurant/create_instances.sh
@@ -7,12 +7,12 @@ NAME=restaurant
 rm -rf $OUTDIR
 mkdir $OUTDIR
 
-for numtable in 50 100 200
+for numtable in 100 500 1000 1500 2000
 do
     maxplaces=$(($numtable*7))
     init=$(($numtable*2))
     # for totalplaces in $(seq $init 100 $maxplaces)
-    for ratio in 2 3 4 5 6
+    for ratio in 2 4 6
     do
         totalplaces=$((numtable*ratio))
         OUTFILE=$OUTDIR/${NAME}${numtable}_${totalplaces}.coom
