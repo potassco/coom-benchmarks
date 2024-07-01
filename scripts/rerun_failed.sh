ee0f9c8cf06a5ddd32b065466c407b85a710090a
Nicolas Rühling <nruehling@uni-potsdam.de>
2024-07-01 23:24:34 +0200

    new core results

--- scripts/rerun_failed.sh
+++ scripts/rerun_failed.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
 DOMAIN=$1
 
 source scripts/vars.env
 
-echo "Re-running failed instances"
+echo "Re-running failed instances of ${DOMAIN}"
 echo
 
 echo "Entering benchmark-tool directory"
@@ -12,17 +18,16 @@ cd $BT_DIR
 
 echo "Removing .finished files of failed instances"
 
-for i in $(find $1 -name "runsolver.watcher" -exec grep -q "runlim error" {} \; -print); do
-    print $i
-    # rm -f "${i/runsolver.watcher/".finished"}"
+for i in $(find . -name "runsolver.watcher" -exec grep -q "runlim error" {} \; -print); do
+    rm -f "${i/runsolver.watcher/".finished"}"
 done
 
-# echo "Generating new run scripts"
-# ./bgen $RUNSCRIPT_PATH
+echo "Generating new run scripts"
+./bgen $RUNSCRIPT_PATH
 
-# echo "Running benchmarks"
-# if [ $MODE == "pbs" ]; then
-#     ./${OUTPUT_DIR}/${DOMAIN}/${PROJECT}/${MACHINE}/start.sh
-# else
-#     echo "Invalid mode"
-# fi
+echo "Running benchmarks"
+if [ $MODE == "pbs" ]; then
+    ./${OUTPUT_DIR}/${DOMAIN}/${PROJECT}/${MACHINE}/start.sh
+else
+    echo "Invalid mode"
+fi
