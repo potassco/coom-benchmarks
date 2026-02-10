8260bf5bd56288529fc08aa9f1942377c48b7868
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-02-10 22:14:01 +0100

    updated bt. updated city and travel bike benchmarks.

--- benchmarks/city-bike-fleet/create_instances.sh
+++ benchmarks/city-bike-fleet/create_instances.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 TEMPLATE=model.coom
-OUTDIR=instances
+MODELDIR=models
+INSTANCEDIR=instances
 NAME=citybike
 
-rm -rf $OUTDIR
-mkdir $OUTDIR
+rm -rf $MODELDIR
+mkdir $MODELDIR
 
-for numBikes in $(seq 10 10 150)
+rm -rf $INSTANCEDIR
+mkdir $INSTANCEDIR
+
+for maxBikes in 50 100 150 200
 do
-    outFile=$OUTDIR/${NAME}-n${numBikes}.coom
-    sed -e "s/NUMBIKES/${numBikes}/g" $TEMPLATE > $outFile
+    modelName=${NAME}-max${maxBikes}
+    modelFile=$MODELDIR/${modelName}.coom
+    sed -e "s/MAXBIKES/${maxBikes}/g" $TEMPLATE > $modelFile
+
+    for ratio in 4 3 2 1
+    do
+        numBikes=$((maxBikes/ratio))
+
+        userFile=$INSTANCEDIR/${modelName}_user-input-n${numBikes}.coom
+        for n in $(seq 0 $(($numBikes-1)))
+        do
+            echo "add bikes[${n}]" >> $userFile
+        done
+    done
 done
