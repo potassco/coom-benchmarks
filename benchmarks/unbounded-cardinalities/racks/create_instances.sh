97047b5c9df71868264fe2858bdf8fbb293287c8
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-02-24 20:35:19 +0100

    updated racks model with associations and added instances

--- benchmarks/unbounded-cardinalities/racks/create_instances.sh
+++ benchmarks/unbounded-cardinalities/racks/create_instances.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-MODELDIR=models
+# MODELDIR=models
 INSTANCEDIR=instances
 
-rm -rf $MODELDIR
-mkdir $MODELDIR
+# rm -rf $MODELDIR
+# mkdir $MODELDIR
 
 rm -rf $INSTANCEDIR
 mkdir $INSTANCEDIR
 
-# Simple model benchmarks
-TEMPLATE=model-simple.coom
-NAME=racks-simple
+# # Simple model benchmarks
+# TEMPLATE=model-simple.coom
+# NAME=racks-simple
 
-for max in 10 20 30; do
-    modelName=${NAME}-${max}
-    modelFile=$MODELDIR/${modelName}.coom
-    sed -e "s/MAX/${max}/g" $TEMPLATE > $modelFile
+for num_elements in {1..20}
+do
+    userFile=$INSTANCEDIR/racks_user-input-${num_elements}.coom
+    touch $userFile
 
-    steps=10
-    step=$((max / steps))
-    for ((i = 0; i < steps; i++)); do
-        numElements=$((1 + i*step))
-        userFile=$INSTANCEDIR/${modelName}_user-input-${numElements}.coom
-        echo "set numA[0] = ${numElements}" > $userFile
-        echo -e "set numB[0] = ${numElements}" >> $userFile
+    i=0
+    for type in A B C D; do
+        for j in $(seq 0 $(($num_elements-1))); do
+            current_element="elements[${i}]"
+            echo "add $current_element" >> $userFile
+            echo "set $current_element.type[0]=$type" >> $userFile
+            i=$(($i+1))
+        done
     done
 done
 
-# Complex model benchmarks
-TEMPLATE=model-complex.coom
-NAME=racks-complex
-
-max=10
-modelName=${NAME}-${max}
-modelFile=$MODELDIR/${modelName}.coom
-sed -e "s/MAX/${max}/g" $TEMPLATE > $modelFile
-
-for numElements in 1 2 3 4 5 6 7 8 9 10; do
-    userFile=$INSTANCEDIR/${modelName}_user-input-${numElements}.coom
-        echo "set numA[0] = ${numElements}" > $userFile
-        echo -e "set numB[0] = ${numElements}" >> $userFile
-        echo -e "set numC[0] = ${numElements}" >> $userFile
-        echo -e "set numD[0] = ${numElements}" >> $userFile
-done
