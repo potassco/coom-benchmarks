1d43b7f3b65a7d33f68ce0b6f6243a7d6354b427
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-02-25 22:29:05 +0100

    added new model and instances for house problem

--- benchmarks/unbounded-cardinalities/house/create_instances.sh
+++ benchmarks/unbounded-cardinalities/house/create_instances.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-MODELDIR=models
+# MODELDIR=models
 INSTANCEDIR=instances
 NAME=house
 
-rm -rf $MODELDIR
-mkdir $MODELDIR
+# rm -rf $MODELDIR
+# mkdir $MODELDIR
 
 rm -rf $INSTANCEDIR
 mkdir $INSTANCEDIR
 
-numInstances=10
-things=10
-roomsPerPerson=5
+maxThings=5
 
-for persons in 1 2 3 4 5; do
-    maxRooms=$((roomsPerPerson * persons))
+for numPersons in $(seq 10 10 200); do
+    userFile=$INSTANCEDIR/house_user-input-${numPersons}.coom
+    touch $userFile
 
-    python generate.py --maxrooms ${maxRooms} --maxthings ${things} --persons ${persons} --instances ${numInstances} --instancedir $INSTANCEDIR --modeldir $MODELDIR
+    for p in $(seq 0 $((numPersons-1))); do
+        current_person="persons[${p}]"
+        echo "add $current_person" >> $userFile
+
+        maxShortThings=$((1 + $RANDOM % $maxThings))
+        for t in $(seq 0 $((maxShortThings-1))); do
+            current_thing="${current_person}.things[${t}]"
+            echo "add $current_thing" >> $userFile
+
+            echo "set $current_thing=Short" >> $userFile
+        done
+
+        maxLongThings=$((1 + $RANDOM % $maxThings))
+        for t in $(seq 0 $((maxLongThings-1))); do
+            current_thing="${current_person}.things[${t}]"
+            echo "add $current_thing" >> $userFile
+
+            echo "set $current_thing=Long" >> $userFile
+        done
+    done
 done
