0ec8f38a8354a1f02ab8a8bc77a8a321a233a746
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-03-31 12:27:23 +0200

    added unbounded models for travel bike and restaurant

---
+++ benchmarks/unbounded-cardinalities/restaurant/create_instances.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TEMPLATE=model.coom
+MODELDIR=models
+INSTANCEDIR=instances
+NAME=restaurant
+
+rm -rf $MODELDIR
+mkdir $MODELDIR
+
+rm -rf $INSTANCEDIR
+mkdir $INSTANCEDIR
+
+
+
+for numtable in 100 500 1000 1500 2000
+do
+    maxplaces=$(($numtable*7))
+    init=$(($numtable*2))
+
+    MODELFILE=$MODELDIR/${NAME}-${numtable}.coom
+    sed -e "s/MAXPLACES/${maxplaces}/g" $TEMPLATE > $MODELFILE
+
+    # for totalplaces in $(seq $init 100 $maxplaces)
+    for ratio in 2 4 6
+    do
+        requiredplaces=$((numtable*ratio))
+        USERFILE=$INSTANCEDIR/${NAME}-${numtable}_user-input-${requiredplaces}.coom
+        echo "set totalPlaces[0] = ${requiredplaces}" > $USERFILE
+    done
+done
---
+++ benchmarks/unbounded-cardinalities/travel-bike-fleet/create_instances.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TEMPLATE=model.coom
+MODELDIR=models
+INSTANCEDIR=instances
+NAME=travelbike
+
+rm -rf $MODELDIR
+mkdir $MODELDIR
+
+rm -rf $INSTANCEDIR
+mkdir $INSTANCEDIR
+
+for maxBikes in 5 10 15 20
+do
+    maxPrice=$((100*maxBikes))
+    maxVolume=$((100*maxBikes))
+    modelName=${NAME}-max${maxBikes}
+    modelFile=$MODELDIR/$modelName.coom
+    sed -e "s/MAXPRICE/${maxPrice}/g" -e "s/MAXVOLUME/${maxVolume}/g" $TEMPLATE > $modelFile
+
+    for i in {1..10}
+    do
+        userVolume=$((1 + $RANDOM % $maxVolume))
+        userPrice=$((1 + $RANDOM % $maxPrice))
+
+        userFile=$INSTANCEDIR/${modelName}_user-input-p${userPrice}-v${userVolume}.coom
+
+        echo "set maxPrice[0] = ${userPrice}" >> $userFile
+        echo "set requestedVolume[0] = ${userVolume}" >> $userFile
+    done
+done
