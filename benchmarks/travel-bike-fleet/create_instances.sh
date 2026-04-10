cc5d36bf2ab94fd3efe1c01614fe2f9e8bf7a3fd
Nicolas Rühling <nruehling@uni-potsdam.de>
2026-04-10 12:58:46 +0200

    fixed travel bike model

--- benchmarks/travel-bike-fleet/create_instances.sh
+++ benchmarks/travel-bike-fleet/create_instances.sh
@@ -8,25 +8,26 @@ NAME=travelbike
 rm -rf $MODELDIR
 mkdir $MODELDIR
 
-rm -rf $INSTANCEDIR
-mkdir $INSTANCEDIR
+# rm -rf $INSTANCEDIR
+# mkdir $INSTANCEDIR
 
 for maxBikes in 5 10 15 20
 do
+    # minPrice=$((15*maxBikes))
     maxPrice=$((100*maxBikes))
     maxVolume=$((100*maxBikes))
     modelName=${NAME}-max${maxBikes}
     modelFile=$MODELDIR/$modelName.coom
     sed -e "s/MAXBIKES/${maxBikes}/g" -e "s/MAXPRICE/${maxPrice}/g" -e "s/MAXVOLUME/${maxVolume}/g" $TEMPLATE > $modelFile
 
-    for i in {1..10}
-    do
-        userVolume=$((1 + $RANDOM % $maxVolume))
-        userPrice=$((1 + $RANDOM % $maxPrice))
+    # for i in {1..10}
+    # do
+    #     userVolume=$(shuf -i 1-$maxVolume -n 1)
+    #     userPrice=$(shuf -i 1-$maxPrice -n 1)
 
-        userFile=$INSTANCEDIR/${modelName}_user-input-p${userPrice}-v${userVolume}.coom
+    #     userFile=$INSTANCEDIR/${modelName}_user-input-p${userPrice}-v${userVolume}.coom
 
-        echo "set maxPrice[0] = ${userPrice}" >> $userFile
-        echo "set requestedVolume[0] = ${userVolume}" >> $userFile
-    done
+    #     echo "set maxPrice[0] = ${userPrice}" >> $userFile
+    #     echo "set requestedVolume[0] = ${userVolume}" >> $userFile
+    # done
 done
